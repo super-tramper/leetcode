78892b43f2df7db716710bc63c1f09acda42c1a4
staphaness@163.com <staphaness@163.com>
2021-01-24 22:51:08 +0800

    435. 无重叠区间 动态规划

--- eraseOverlapIntervals.py
+++ eraseOverlapIntervals.py
@@ -1,5 +1,14 @@
 def eraseOverlapIntervals(intervals) -> int:
-    """从区间列表中选出尽量多的区间，它们之间没有重叠，剩下的就是要移除的区间"""
-    count = 0
-    for i in len(intervals+1):
-        pass
+    n = len(intervals)
+    if not n:
+        return 0
+    f = [1]
+    intervals.sort()
+    for i in range(1, n):
+        f.append(max([f[j] for j in range(i) if intervals[j][1] <= intervals[i][0]], default=0) + 1)
+    return n - max(f)
+
+
+if __name__ == '__main__':
+    intervals = [[1, 2], [2, 3], [3, 4], [1, 3]]
+    print(eraseOverlapIntervals(intervals))
