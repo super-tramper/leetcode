059bb3cae14e682379dfb2bde7ad31234bde6999
staphaness@163.com <staphaness@163.com>
2021-02-01 19:53:41 +0800

    888. 公平的糖果棒交换

---
+++ code/fairCandySwap.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fairCandySwap(self, A, B):
+        sum1 = sum(A)
+        sum2 = sum(B)
+        margin = (sum1 - sum2) // 2
+        ans = []
+        b = set(B)
+        for i in A:
+            j = i - margin
+            if j in b:
+                ans.append(i)
+                ans.append(j)
+        return ans
+
+
+if __name__ == '__main__':
+    A = [1, 1]
+    B = [2, 2]
+    solution = Solution()
+    print(solution.fairCandySwap(A, B))
