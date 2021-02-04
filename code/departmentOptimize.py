ac0e947c4584f6b779dd55cbdf3186ebed8e0a48
staphaness@163.com <staphaness@163.com>
2021-02-04 19:19:36 +0800

    添加注释

--- code/departmentOptimize.py
+++ code/departmentOptimize.py
@@ -1,9 +1,11 @@
+# 从中间生成的结果来看，第三次优化后，各组的人数分别为8 5 7 6，再进行4次优化后，各组人数再次变成 8 5 7 6
+# 中间有 115 次优化可省略，从开始到最终结果只需5次优化
 def optimize(nums, k):
     n = len(nums)
     for i in range(k):
         _, maxInd = getMaxIndex(nums, n)
-        # print(maxInd)
-        # print(nums)
+        print(maxInd)
+        print(nums)
         for j in range(n):
             nums[j] = nums[j] - 3 if j == maxInd else nums[j] + 1
     return nums
