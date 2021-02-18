c926564786a7d46d80e9d04a648f2146905265ba
staphaness@163.com <staphaness@163.com>
2021-02-18 20:26:36 +0800

    995. K 连续位的最小翻转次数 73%

--- code/array/minKBitFlips.py
+++ code/array/minKBitFlips.py
@@ -4,20 +4,21 @@ from typing import List
 class Solution:
     def minKBitFlips(self, A: List[int], K: int) -> int:
         n = len(A)
-        ans = 0
-        for i in range(n-K+1):
-            if not A[i]:
-                self.swap(A, i, K)
+        differs = [0] * (n + 1)
+        ans, revCnt = 0, 0
+        for i in range(n):
+            revCnt += differs[i]
+            if (A[i] + revCnt) % 2 == 0:
+                if i + K > n:
+                    return -1
+                revCnt += 1
                 ans += 1
-        return ans if len(set(A[-K:])) == 1 else -1
-
-    def swap(self, A, start, K):
-        for i in range(K):
-            A[start+i] = 0 if A[start+i] else 1
+                differs[i + K] -= 1
+        return ans
 
 
 if __name__ == '__main__':
-    ar = [1,1,0]
-    k = 2
+    ar = [0, 0, 0, 1, 0, 1, 1, 0]
+    k = 3
     solution = Solution()
     print(solution.minKBitFlips(ar, k))
