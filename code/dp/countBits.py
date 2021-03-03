d1ca55ebb4375eb33b7f63160985028cbca9ef70
staphaness@163.com <staphaness@163.com>
2021-03-03 20:52:50 +0800

    338. 比特位计数 49%

--- code/dp/countBits.py
+++ code/dp/countBits.py
@@ -3,15 +3,15 @@ from typing import List
 
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        # ans = [0]
-        # for i in range(1, num + 1):
-        #     # i & (i-1) 去掉了i最右边的1(若有)
-        #     ans.append(ans[i & (i - 1)] + 1)
-        # return ans
         ans = [0]
         for i in range(1, num + 1):
-            ans.append(ans[i >> 1] + (i & 1))
+            # i & (i-1) 去掉了i最右边的1(若有)
+            ans.append(ans[i & (i - 1)] + 1)
         return ans
+        # ans = [0]
+        # for i in range(1, num + 1):
+        #     ans.append(ans[i >> 1] + (i & 1))
+        # return ans
 
 
 if __name__ == '__main__':
