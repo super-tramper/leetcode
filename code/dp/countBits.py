1fcf2cd48b8009b9ea7f9136b30d59df177d00b7
staphaness@163.com <staphaness@163.com>
2021-03-03 20:42:58 +0800

    338. 比特位计数 97%

---
+++ code/dp/countBits.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        ans = [0]
+        for i in range(1, num + 1):
+            # i & (i-1) 去掉了i最右边的1(若有)
+            ans.append(ans[i & (i - 1)] + 1)
+        return ans
+
+
+if __name__ == '__main__':
+    num = 5
+    solution = Solution()
+    print(solution.countBits(num))
