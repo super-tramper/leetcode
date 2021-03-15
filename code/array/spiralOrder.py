d2c8f05f078783151811a53d3c72e2ce53e66964
staphaness@163.com <staphaness@163.com>
2021-03-15 20:26:33 +0800

    54. 螺旋矩阵 medium 没太大意思

---
+++ code/array/spiralOrder.py
@@ -0,0 +1,39 @@
+"""
+给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[1,2,3,6,9,8,7,4,5]
+
+初步思路为列出顺时针的四个方向，借助标记矩阵，不断遍历
+官方题解中从外层到内层的遍历思路更节省空间，以下为官方实现代码
+"""
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return list()
+        
+        rows, columns = len(matrix), len(matrix[0])
+        visited = [[False] * columns for _ in range(rows)]
+        total = rows * columns
+        order = [0] * total
+
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        row, column = 0, 0
+        directionIndex = 0
+        for i in range(total):
+            order[i] = matrix[row][column]
+            visited[row][column] = True
+            nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
+            if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+                directionIndex = (directionIndex + 1) % 4
+            row += directions[directionIndex][0]
+            column += directions[directionIndex][1]
+        return order
+
+
+if __name__ == '__main__':
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    solution = Solution()
+    print(solution.spiralOrder(matrix))
